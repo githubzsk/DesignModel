92f3219fb74866c540144c3cafa1b244ac8f6f15
zz <zzzsk163@163.com>
2020-06-06 17:32:23 +0800

    -m

--- src/com/xintong/code/strategy/strategy3/SpringCloudArticle.java
+++ src/com/xintong/code/strategy/strategy3/SpringCloudArticle.java
@@ -4,10 +4,10 @@ public enum SpringCloudArticle implements Article {
     GETINSTANCE(0,""),
     ITEM1(1,"1 EurekaServer"),//
     ITEM2(2,"2 EurekaClient"),//
-    ITEM3(3,"3 Eureka"),//
+    ITEM3(3,"3 Eureka"),//1
     ITEM4(4,"4 EurekaServer缓存机制"),//
     ITEM5(5,"5 注册表"),//
-    ITEM6(6,"6 Eureka高可用原理"),//
+    ITEM6(6,"6 Eureka高可用原理"),//1
     ITEM7(7,"7 Eureka AP原理"),//
     ITEM8(8,"8 Eureka服务注册原理"),//1
     ITEM9(9,"9 eureka和zookeeper作为注册中心的区别"),//
